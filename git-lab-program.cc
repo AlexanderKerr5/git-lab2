945392b2a3fa66d40d2061e0c2f6d33c8e87f2bf
Alexander Kerr <ak124620@ohio.edu>
2021-01-29 15:20:56 -0500

    Program commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Alexander Kerr
+ *        Date: 1/29/21
+ * Description: Git lab step 15's program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
